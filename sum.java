104950dda8cda70a7719c32121fa43326e1212ed
Aswinig18 <gaswini313@gmail.com>
2017-06-12 10:39:37 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class sum
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int t=s.nextInt();
+int a=0;
+for(int i=1;i<=t;i++)
+{
+    a=a+i;
+}
+System.out.println(a);
+}
+}
